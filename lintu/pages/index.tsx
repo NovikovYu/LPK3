09344281e9e54a638b9ace9a63b8c19617067c4c
Diana <diana@MacBook-Pro-Diana.local>
2023-05-02 18:27:09 +0300

    add header

--- lintu/pages/index.tsx
+++ lintu/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import * as React from 'react';
+import { Container, Typography } from '@mui/material';
+import Header from '@/components/Layout/header';
 
 export default function Home() {
   return (
@@ -7,10 +10,13 @@ export default function Home() {
         <title>Lintu</title>
         <meta name="description" content="Lintu" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+  
       </Head>
       <main>
-       
+      <Header />
+      <Container maxWidth='lg' sx={{p:0, m:0}}>
+        <Typography sx={{fontFamily: 'Roboto', fontSize:24,  pt:4.875, pl:13.5}}>Home Page</Typography>
+      </Container>
       </main>
     </>
   )
