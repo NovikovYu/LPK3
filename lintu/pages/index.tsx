ca18273be7b8f68325b79c509c6df9ff7870bcc0
Diana <diana@MacBook-Pro-Diana.local>
2023-05-02 17:47:13 +0300

    test

---
+++ lintu/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Lintu</title>
+        <meta name="description" content="Lintu" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+       
+      </main>
+    </>
+  )
+}
