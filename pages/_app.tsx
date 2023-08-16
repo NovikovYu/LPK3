c50fc53305f073d5524fbd7c921311ef2e2d3b50
Юрий Новиков <ynovikov@nologostudio.ru>
2023-08-16 08:44:12 +0300

    fix/layout-refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -147,8 +147,8 @@ const theme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
   );
 }
--- pages/start-the-questionnaire.tsx
+++ pages/start-the-questionnaire.tsx
@@ -2,10 +2,35 @@ import { Container } from '@mui/material';
 import Layout from '@/components/Layout/Layout';
 import { MainBox } from '@/components/CommonComponents/Common-сomponents-style';
 import InfoPageContent from '@/components/Info-page/info-page-content';
+import RootLayout from '@/components/Layout/RootLayout';
+
+// import { Metadata } from 'next'
+ 
+// export const metadata: Metadata = {
+//   title: 'Page With META title',
+//   description: 'Page With META description',
+// }
 
 export default function StartTheQuestionnaire() {
   return (
-    <Layout pageTitle={'Lintu - start the questionnaire page'}>
+    // <Layout pageTitle={'Lintu - start the questionnaire page'}>
+    //   <MainBox component="main">
+    //     <Container sx={{ maxWidth: '808px' }} maxWidth={false}>
+    //       <InfoPageContent
+    //         imgSrc={'/business-product-planning-and-research.png'}
+    //         imgAlt={
+    //           'our analytics are thinking about the best investment strategy for you'
+    //         }
+    //         titleText={'Creating a new investment portfolio'}
+    //         mainText={
+    //           "Answer 38 questions and we'll create a unique, personalized portfolio for you"
+    //         }
+    //         buttonText={'Start the questionnaire'}
+    //         buttonLink={'/survey'}
+    //       />
+    //     </Container>
+    //   </MainBox>
+    // </Layout>
       <MainBox component="main">
         <Container sx={{ maxWidth: '808px' }} maxWidth={false}>
           <InfoPageContent
@@ -22,6 +47,5 @@ export default function StartTheQuestionnaire() {
           />
         </Container>
       </MainBox>
-    </Layout>
   );
 }
